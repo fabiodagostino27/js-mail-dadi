21f47d4622b14fa5f60a62cd16985b053f9d461f
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-14 12:37:46 +0100

    ADD: 2 consts

---
+++ Dadi/main.js
@@ -0,0 +1,7 @@
+alert("Clicca OK per tirare il dado!")
+const x = Math.floor((Math.random() * 6) + 1);
+alert(`Il tuo numero è ${x}. Ora tocca a me tirare!`)
+console.log(`Il tuo numero è ${x}`)
+
+const y = Math.floor((Math.random() * 6) + 1)
+
