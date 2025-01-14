84368b29566aa587c3044f0f3358962d9e629da8
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-14 12:01:53 +0100

    ADD: array and prompt

---
+++ Mail/main.js
@@ -0,0 +1,15 @@
+const emails = [
+    "pippo1@gmail.com",
+    "pippo2@gmail.com",
+    "pippo3@gmail.com",
+    "pippo4@gmail.com",
+    "pippo5@gmail.com",
+    "pippo6@gmail.com",
+    "pippo7@gmail.com",
+    "pippo8@gmail.com",
+    "pippo9@gmail.com",
+    "pippo10@gmail.com"
+]
+
+let yourEmail = prompt("Qual è la tua email?") == 0
+
